a30f7e365ad9f52593eff9f6d8585ed69befa6c9
okaylesgo <20004@ECAM.BE>
2022-04-28 11:11:14 +0200

    inscription

---
+++ inscription.py
@@ -0,0 +1,19 @@
+import socket
+import asyncio
+import json
+port=int(1234)
+Addressplayer = ('0.0.0.0', port)
+servoraddress=('localhost',3000)
+def inscription():
+        print('test')
+        with socket.socket() as s:
+            s.connect(servoraddress)
+            inscription={"request": "subscribe","port": port,"name":'akachar Ali',"matricules":["20004",'20009']}
+            message=json.dumps(inscription)
+            s.send(message.encode())
+            message=s.recv(2048).decode()
+            print(message)
+            if message=='{"response": "ok"}':
+                print('OK ON COMMENCE')
+            else:
+                raise ValueError("Inscription Error: " + message)
